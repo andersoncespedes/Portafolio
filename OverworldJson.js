7dddac601c7033acffe1d9063feb2f7ebbbc9d65
anderson cespedes <andersontrabajos2@gmail.com>
2023-11-16 16:33:20 +0000

    feat: :sparkles: Creacion de animaciones de personaje

--- OverworldJson.js
+++ OverworldJson.js
@@ -1,10 +1,18 @@
 export const overworldJson = {
     heroe:{
-        frameX:0,
-        frameY:0,
+        frameX:1,
+        frameY:10,
         width:16,
         height:24,
-        spriteImg:"./Assets/Sprites/Ness.png"
+        spriteImg:"./Assets/Sprites/Ness.png",
+        animationSprite:
+        {
+            "standDown":[[0,0]],
+            "movUp":[[0,3], [0,5]],
+            "movDown":[[0,0],[0,1]],
+            "movRight":[[0,4], [0,5]],
+            "movLeft":[[1,3],[1,2]]
+        }
     },
     level:[
     {
--- Sprites/index.js
+++ Sprites/index.js
@@ -10,11 +10,15 @@ export class Sprite {
     this.img = param.spriteImg;
     this.frameX = param.frameX;
     this.frameY = param.frameY;
+    this.animationSelect = "movDown";
+    this.frameActual = 0;
+    this.WaitAnimation = 10;
   }
  
   draw() {
     let Img = new Image();
     Img.src = this.img;
+    this.animation();
     Img.onload = () => {
       this.ctx.drawImage(
         Img,
@@ -28,7 +32,23 @@ export class Sprite {
         22
       );
     };
-
+  }
+  animation(){
+    this.frameX = 16 * this.param.animationSprite[this.animationSelect][this.frameActual][1];
+    this.frameY = 23 * this.param.animationSprite[this.animationSelect][this.frameActual][0];
+    if(this.WaitAnimation > 0){
+      this.WaitAnimation--;
+    }
+    else{
+      if(this.frameActual < 1){
+      this.frameActual++;
+      }
+      else{
+        this.frameActual = 0;
+      }
+      this.WaitAnimation = 10;
+    }
+    
   }
   main(){
     this.draw();
